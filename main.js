116c0ae6f772e282e5d8411a059ec16ccf874a93
CCTV19 <835202378@qq.com>
2022-06-05 14:26:03 +0800

    完成了首页功能

--- main.js
+++ main.js
@@ -2,6 +2,28 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+
+uni.$showMsg = function(title = '数据请求失败!',duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
 
 Vue.config.productionTip = false
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,40 @@
 <template>
 	<view>
-		Home
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
+			<swiper-item v-for="(item,i) in swiperList" :key="i">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+					<image :src="item.image_src"></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item ,i) in navList" :key="i" @click="navClickHandler(item)">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
+		
+		<view class="floor-list">
+			<view class="floor-item" v-for="(item,i) in floorList" :key="i">
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<view class="floor-img-box">
+					<view class="left-img-box">
+						<image :src="item.product_list[0].image_src" :style="{width:
+						item.product_list[0].image_width + 'rpx'}" 
+						mode="widthFix"></image>
+					</view>
+					<view class="right-img-box">
+						<view class="right-img-item" 
+						v-for="(item2,i2) in item.product_list" :key="i2" 
+						v-if="i2 !== 0">
+							<image :src="item2.image_src" 
+							:style="{width:item2.image_width + 'rpx'}" 
+							mode="widthFix"></image>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +42,74 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList:[],
+				navList:[],
+				floorList:[]
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+			this.getNavList()
+			this.getFloorList()
+		},
+		methods:{
+			async getSwiperList(){
+				const { data : res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+				console.log(res)
+				if(res.meta.status !== 200) return uni.$showMsg()
+				this.swiperList = res.message
+			},
+			async getNavList(){
+				const {data : res} = await uni.$http.get('/api/public/v1/home/catitems')
+				if(res.meta.status !== 200) return uni.$showMsg()
+				this.navList = res.message
+			},
+			async getFloorList(){
+				const {data : res} = await uni.$http.get('/api/public/v1/home/floordata')
+				if(res.meta.status !== 200) return uni.$showMsg()
+				this.floorList = res.message
+			},
+			navClickHandler(item){
+				if(item.name === '分类'){
+					uni.switchTab({
+						url:'/pages/cate/cate'
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	swiper{
+		height: 330rpx;
+		.swiper-item,
+		image{
+			width: 100%;
+			height: 100%;
+		}
+	}
+	.nav-list{
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0 ;
+		.nav-img{
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+	.floor-title{
+		width: 100%;
+		height: 60rpx;
+	}
+	.right-img-box{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		padding-left: 8rpx;
+	}
+	.floor-img-box{
+		display: flex;
+		padding-left: 10rpx;
+	}
 </style>
